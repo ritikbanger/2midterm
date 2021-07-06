50acf72196a89ac3bf2ffd7cd2eac4cb18b89f91
ritikbanger <47841501+ritikbanger@users.noreply.github.com>
2021-07-06 14:32:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,8 +50,7 @@ def main():
    <div class="clearfix">           
    <div class="col-md-12">
    <center><p style="font-size:40px;color:white;margin-top:10px;">Poornima Institute of Engineering & Technology</p></center> 
-   <center><p style="font-size:30px;color:white;margin-top:10px;">Department of Computer Engineering</p></center> 
-   <center><p style="font-size:25px;color:white;margin-top:10px;">Deep Learning  Lab Experiment Deployment</p></center> 
+   <center><p style="font-size:30px;color:white;margin-top:10px;">Department of Computer Engineering</p></center>
    </div>
    </div>
    </div>
